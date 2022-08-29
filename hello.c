f11628fc0067efa11bb75188f84c3e8e25e6e59a
DBui4 <111161486+DBui4@users.noreply.github.com>
2022-08-29 11:00:25 -0500

    added name and proposal

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@ int main(int argc, char **argv) {
     printf("Hello, I'm Seamus McNear!\n");
     printf("Major is Computer Science!\n");
     printf("Minor is Economics\n");
+    printf("Hello, I'm Dennis Bui!\n");
+    printf("Major is Computer Science!\n");
 
     return 0;
 }
